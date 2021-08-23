83b7838b48bb845c1279da913f37b220c29e4f53
omar.barrios <omar.barrios@alumnos.uneatlantico.es>
2021-08-23 22:36:36 +0200

    [develop] proyecto base, del curso

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # web-server
 
-Proyecto sencillo de un servidor web con Node js 
+Proyecto sencillo de un servidor web con Nodejs y Express. 
+
+## Comandos del proyecto
+
+```
+npm run dev
+```
+
+```
+npm run start
+```
+
+## Variables de entorno
+
+Esta aplicación necesita las siguientes variables de entorno:
+
+- ` PORT `
+
+## Recursos
+
+- [Nodejs](https://nodejs.org/es/)
+- [npm](https://www.npmjs.com/)
+- [Express](https://expressjs.com/es/)
+
+
+###### ⌨️ by [omar.barrios]()
---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express')
+const hbs = require('hbs');
+
+
+const app = express()
+const port = process.env.PORT
+
+//Handlebars
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//servir contenido estatico.
+app.use( express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Omar Barrios',
+        titulo: 'Web Server'
+    });
+})
+
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'Omar Barrios',
+        titulo: 'Web Server'
+    });
+})
+
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'Omar Barrios',
+        titulo: 'Web Server'
+    });
+})
+
+app.get('*', (req, res) => {
+  res.sendFile( __dirname + '/public/404.html');
+})
+ 
+app.listen( port, () => {
+    console.log(`Web-server listening at http://localhost:${ port }`);
+})
