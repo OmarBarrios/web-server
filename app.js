e6a69f2947141b17a4e02178fa12142b78680725
omar.barrios <omar.barrios@alumnos.uneatlantico.es>
2021-08-25 15:51:24 +0200

    [fix] cambios en la informacion de las vistas

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-require('dotenv').config();
-const express = require('express')
-const hbs = require('hbs');
+import dotenv from 'dotenv'
+import express from 'express';
+import hbs from 'hbs';
+import { fileURLToPath } from 'url'
+import path from 'path'
+import { dirname } from 'path'
+
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express()
 const port = process.env.PORT
@@ -12,26 +21,26 @@ app.set('view engine', 'hbs');
 //servir contenido estatico.
 app.use( express.static('public'));
 
-// app.get('/', (req, res) => {
-//     res.render('home', {
-//         nombre: 'Omar Barrios',
-//         titulo: 'Web Server'
-//     });
-// })
+ app.get('/', (req, res) => {
+     res.render('home', {
+         nombre: 'Omar Barrios',
+         titulo: 'Web Server'
+     });
+ })
 
-// app.get('/generic', (req, res) => {
-//     res.render('generic', {
-//         nombre: 'Omar Barrios',
-//         titulo: 'Web Server'
-//     });
-// })
+ app.get('/generic', (req, res) => {
+     res.render('generic', {
+         nombre: 'Omar Barrios',
+         titulo: 'Web Server'
+     });
+ })
 
-// app.get('/elements', (req, res) => {
-//     res.render('elements', {
-//         nombre: 'Omar Barrios',
-//         titulo: 'Web Server'
-//     });
-// })
+ app.get('/elements', (req, res) => {
+     res.render('elements', {
+         nombre: 'Omar Barrios',
+         titulo: 'Web Server'
+     });
+ })
 
 app.get('*', (req, res) => {
   res.sendFile( __dirname + '/public/index.html');
